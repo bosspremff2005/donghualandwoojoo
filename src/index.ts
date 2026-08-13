243578f6c2f44dd4a41cbc9d4f728ffbf5db8e96
bosspremff2005 <bosspremff067@gmail.com>
2026-08-13 21:06:24 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -298,7 +298,7 @@ app.get('/', async (c) => {
 })
 
 // Anime detail page
-app.get('/anime/:slug', async (c) => {
+app.get('/drama/:slug', async (c) => {
   const slug = c.req.param('slug')
   const db = c.env?.DB
 
